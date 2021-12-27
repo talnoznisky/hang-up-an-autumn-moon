10768f987436379b3a1e594ac315be0cd45114a5
talnoznisky <tal.noznisky@gmail.com>
2021-12-27 15:35:21 -0600

    remove extraneous files

--- hang_up_an_autumn_moon_over_the_mountain/scratch/import_test.py
+++
@@ -1,7 +0,0 @@
-from hang_up_an_autumn_moon_over_the_mountain.oracle.main import Oracle
-
-o = Oracle()
-digests = o.create_digests()
-
-for digest in digests:
-    print(digest)
--- hang_up_an_autumn_moon_over_the_mountain/scratch/main.py
+++
@@ -1,9 +0,0 @@
-from random_art.randomart import draw, drunkenwalk
-...
-
-# generate your hash digest
-digest = 'l;kasdasdlk'
-
-# generate randomart, HASHNAME must be 10 characters
-art = draw(drunkenwalk(digest))
-print(art)

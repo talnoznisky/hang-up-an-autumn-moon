082ba17b8b48db725a56e55adf1a256a1e1a02d0
talnoznisky <tal.noznisky@gmail.com>
2022-01-14 22:16:28 -0600

    add dependencies for heroku deployment

--- api/main.py
+++ api/main.py
@@ -3,18 +3,8 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from hang_up_an_autumn_moon_over_the_mountain.oracle.main import Oracle
 from hang_up_an_autumn_moon_over_the_mountain.oracle.assets.texts import cards, project_credit, guidebook_text
-from typing import Optional
 
 app = FastAPI()
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,6 +16,7 @@ app.add_middleware(
 
 # text query endpoints
 @app.get("/v1/oracle")
-def get_text(query: str):
-    o = Oracle(sparrow_mode=True).run_oracle('1')
+def get_oracle(sparrow_mode=False):
+    o = Oracle(sparrow_mode)
     return o
+
